cef9553a118c4bda7f9953201a8fe27d16dc4659
xdedm11 <xdedm11@tju.edu.cn>
2021-01-18 19:40:20 +0800

    lab1 complete

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,33 +1,65 @@
-/** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+/**
+ * Class that determines whether or not a year is a leap year.
+ *
+ * @author YOUR NAME HERE
  */
-public class LeapYear {
+public class LeapYear
+{
 
-    /** Calls isLeapYear to print correct statement.
-     *  @param  year to be analyzed
+    /**
+     * Calls isLeapYear to print correct statement.
+     *
+     * @param year to be analyzed
      */
-    private static void checkLeapYear(int year) {
-        if (isLeapYear(year)) {
+    private static void checkLeapYear(int year)
+    {
+        if (isLeapYear(year))
+        {
             System.out.printf("%d is a leap year.\n", year);
-        } else {
+        } else
+        {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
 
-    /** Must be provided an integer as a command line argument ARGS. */
-    public static void main(String[] args) {
-        if (args.length < 1) {
+    /**
+     * Must be provided an integer as a command line argument ARGS.
+     */
+    public static void main(String[] args)
+    {
+        if (args.length < 1)
+        {
             System.out.println("Please enter command line arguments.");
             System.out.println("e.g. java Year 2000");
         }
-        for (int i = 0; i < args.length; i++) {
-            try {
+        for (int i = 0; i < args.length; i++)
+        {
+            try
+            {
                 int year = Integer.parseInt(args[i]);
                 checkLeapYear(year);
-            } catch (NumberFormatException e) {
+            } catch (NumberFormatException e)
+            {
                 System.out.printf("%s is not a valid number.\n", args[i]);
             }
         }
+    }
+
+    /**
+     * To judge a year if a LeapYear
+     */
+    public static boolean isLeapYear(int year)
+    {
+        boolean res = false;
+        if (year % 400 == 0)
+        {
+            res = true;
+        }
+        if (year % 4 == 0 && year % 100 != 0)
+        {
+            res = true;
+        }
+        return res;
     }
 }
 

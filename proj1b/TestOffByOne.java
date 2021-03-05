874e9f8ec196a8e82290f10e1077a0732dd7b84f
xdedm11 <xdedm11@tju.edu.cn>
2021-03-05 10:50:33 +0800

    modify proj1b/TestOffByOne.java

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -14,4 +14,37 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('c', 't'));
     }
 
+    @Test
+    public void testEqualsCharLower() {
+        assertFalse(offByOne.equalChars('a', 'a'));
+        assertTrue(offByOne.equalChars('a', 'b'));
+        assertTrue(offByOne.equalChars('b', 'a'));
+        assertFalse(offByOne.equalChars('a', 'c'));
+        assertFalse(offByOne.equalChars('c', 'a'));
+    }
+
+    @Test
+    public void testEqualsCharUpper() {
+        assertFalse(offByOne.equalChars('A', 'A'));
+        assertTrue(offByOne.equalChars('A', 'B'));
+        assertTrue(offByOne.equalChars('B', 'A'));
+        assertFalse(offByOne.equalChars('A', 'C'));
+        assertFalse(offByOne.equalChars('C', 'A'));
+    }
+
+    @Test
+    public void testEqualsCharNonAlphabet() {
+        assertTrue(offByOne.equalChars('%', '&'));
+        assertTrue(offByOne.equalChars('&', '%'));
+        assertFalse(offByOne.equalChars('$', '&'));
+        assertFalse(offByOne.equalChars('&', '$'));
+    }
+
+    @Test
+    public void testEqualsCharMixed() {
+        assertFalse(offByOne.equalChars('a', 'A'));
+        assertFalse(offByOne.equalChars('a', '%'));
+        assertFalse(offByOne.equalChars('A', '%'));
+    }
+
 }
